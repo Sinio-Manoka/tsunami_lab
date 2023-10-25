50d1ceaa8a5e1cf358ededcb6d6f86a2c4b683d2
Ward <ward.tammaa@uni-jena.de>
2023-10-25 21:01:06 +0200

    Last Update All Bugs Fixed Functions are now written right and more Tests have been added

--- build/src/solvers/fwave.h
+++ build/src/solvers/fwave.h
@@ -13,6 +13,7 @@ class tsunami_lab::solvers::fwave {
   private:
     static t_real constexpr m_gSqrt = 3.131557121;
     static t_real constexpr g = 9.80665;
+
     static void eigenwerte( t_real   i_hL,
                             t_real   i_hR,
                             t_real   i_uL,
@@ -24,15 +25,28 @@ class tsunami_lab::solvers::fwave {
                       t_real i_hR,
                       t_real i_huL,
                       t_real i_huR,
-                      t_real* & o_fdelta);
+                      t_real o_fdelta[2]);
+
+    static void decompose(t_real i_alphas[2],
+                          t_real i_eigens[2],
+                          t_real o_minus_A_deltaQ[2],
+                          t_real o_plus_A_deltaQ[2]);
 
     static void inverseMatrix(t_real i_eigen1,
                               t_real i_eigen2,
                               t_real o_inverse[4]);
+
+    static void eigencoefficientAlpha(t_real i_inverse[4],
+                                      t_real i_spalte[2],
+                                      t_real o_eigencoefficients[2]);
+  
   public:
-    static void netUpdates( t_real i_eigen1,
-                            t_real i_eigen2,
-                            t_real o_inverse[4]);                               
+    static void netUpdates( t_real   i_hL,
+                            t_real   i_hR,
+                            t_real   i_huL,
+                            t_real   i_huR,
+                            t_real   o_minus_A_deltaQ[2],
+                            t_real   o_plus_A_deltaQ[2] );                               
 };
 
 #endif
