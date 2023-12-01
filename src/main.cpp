8fd033dc9ea5a175c3179f9a44829961e8d63c62
Winter <ward.tammaa@gmail.com>
2023-12-01 13:18:59 +0100

    Timer has been added

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,37 @@
 #include <chrono>
 #include <thread>
 
+class Timer {
+  public:
+      Timer() {
+          start_time = std::chrono::high_resolution_clock::now();
+      }
+
+      ~Timer() {
+          end_time = std::chrono::high_resolution_clock::now();
+          auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count();
+
+          int hours = duration / 3600;
+          int minutes = (duration % 3600) / 60;
+          int seconds = duration % 60;
+
+          std::cout << "\nProgram started at: " << getFormattedTime(start_time)
+                    << "\nProgram ended at: " << getFormattedTime(end_time)
+                    << "\nProgram duration: " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds\n";
+      }
+
+  private:
+      std::chrono::high_resolution_clock::time_point start_time;
+      std::chrono::high_resolution_clock::time_point end_time;
+
+      std::string getFormattedTime(const std::chrono::high_resolution_clock::time_point& time) {
+          auto time_t = std::chrono::system_clock::to_time_t(time);
+          std::string time_str = std::ctime(&time_t);
+          time_str.pop_back();  // Remove the trailing newline character
+          return time_str;
+      }
+};
+
 void updateProgressBar(double current, double total, int width = 50) {
     double progress = (current / total) * 100;
     progress = std::min(std::max(progress, 0.0), 100.0);
@@ -35,9 +66,9 @@ void updateProgressBar(double current, double total, int width = 50) {
     std::cout.flush();
 }
 
-
 int main() {
   
+  Timer timer;
   // number of cells in x- and y-direction
   tsunami_lab::t_idx l_nx = 0;
   tsunami_lab::t_idx l_ny = 1;
